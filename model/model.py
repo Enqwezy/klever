857142db7ceeec96fb571464a4023537794f91a4
Enqwezy <kenzeomirtaj@gmail.com>
2025-03-09 12:56:49 +0600

    create user model

---
+++ model/model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, DateTime, func, Column, Integer, Text
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from database.db import Base
+from typing import Optional
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), default="", nullable=True)
+    email = Column(String(100), default="", nullable=True)
+    phone_number = Column(String(20), nullable=True)
+    address = Column(Text, default="", nullable=True)
+    password = Column(String(255), default="", nullable=True)
