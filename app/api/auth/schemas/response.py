e7b5ee2eecf05b07b7e4398f2dd13d18edefee18
Enqwezy <kenzeomirtaj@gmail.com>
2025-05-12 23:08:12 +0600

    Добавил на регистрацию и логин UserResponse

--- app/api/auth/schemas/response.py
+++ app/api/auth/schemas/response.py
@@ -1,7 +1,20 @@
 from json import dump
 from pydantic import BaseModel
+from typing import Optional
 
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: Optional[str]
+    email: Optional[str]
+    phone_number: Optional[str]
+    address: Optional[str]
+
+    class Config:
+        from_attributes = True
 
 class TokenResponse(BaseModel):
     access_token: str
     access_token_expire_time: str
+    user: UserResponse
